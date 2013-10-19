87ec1a486d2d2535a143d036bf5172b3d3f6e85c
Chakradhar Kasturi <chakri.gudboy@gmail.com>
2013-10-19 16:47:40 +0530

    Update content.js

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		// if the value is true only then the loop is executed
         chrome.storage.local.get('overwrite', function (usrvalue) {console.log(typeof usrvalue['overwrite'])});
         chrome.storage.local.get('overwrite', function (usrvalue) {
-            if (usrvalue['overwrite'] != undefined && usrvalue['overwrite'] == "true") {
+            if (usrvalue['overwrite'] != undefined && usrvalue['overwrite']) {
                 var logoid = document.getElementById("hplogo");
                 console.log(logoid);
                 $("#pagelet_ego_pane").remove();
@@ -12,15 +12,15 @@ $(document).ready(function(){
                 $("#hplogo").text("");
                 chrome.storage.local.get('imgHexaData', function (data) {
                     //In google.com there is no div tag where as in the india domain we have div tag
-                    if ($("#hplogo")[0].tagName == 'IMG') {
-						//$("#hplogo").attr('src', data['imgHexaData']);
-                        $("#hplogo").css('background-image', 'url(' + data['imgHexaData'] + ')');
+                   if ($("#hplogo")[0].tagName == 'IMG') {
+                        $("#hplogo").attr('src', data['imgHexaData']);
                         $("#hplogo").css('background-size', "360px");
-                        $("#hplogo").css('width', "320px");
+                        $("#hplogo").css('height', "120px");
                     } else if ($("#hplogo")[0].tagName == 'DIV') {
                         $("#hplogo").css('background-image', 'url(' + data['imgHexaData'] + ')');
                         $("#hplogo").css('background-size', "360px");
                         $("#hplogo").css('width', "320px");
+                        //$("#hplogo").css('background-position-y', "10px");
                     }
                 });
             }
